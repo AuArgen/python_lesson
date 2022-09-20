9d266342adc92944ccbea195f6126376c77299d0
AuArgen <argenazizbekuulu3@gmail.com>
2022-09-20 20:30:21 +0600

    one

---
+++ lesson_1/main.py
@@ -0,0 +1,15 @@
+# a = input("san jaz1 = ")
+# b = input("san jaz2 = ")
+# print( str(a) ,"+", str(b) ,"=" , str(a+b))
+
+# int - bul butun sandar
+
+# a = int(input("san jaz1 = "))
+# b = int(input("san jaz2 = "))
+# print( str(a) ,"+", str(b) ,"=" , str(a+b))
+
+#float - bul anyk sandar (1.6, 5.5) 
+
+a = float(input("san jaz1 = "))
+b = float(input("san jaz2 = "))
+print( str(a) ,"+", str(b) ,"=" , str(a+b))
