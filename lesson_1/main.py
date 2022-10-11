e51ee24d45eb6e127d93e01a413d79a6ec07ab4a
AuArgen <argenazizbekuulu3@gmail.com>
2022-10-11 20:53:51 +0600

    lesson2

--- lesson_1/main.py
+++ lesson_1/main.py
@@ -10,6 +10,18 @@
 
 #float - bul anyk sandar (1.6, 5.5) 
 
-a = float(input("san jaz1 = "))
-b = float(input("san jaz2 = "))
-print( str(a) ,"+", str(b) ,"=" , str(a+b))
+# a = float(input("san jaz1 = "))
+# b = float(input("san jaz2 = "))
+# print( str(a) ,"+", str(b) ,"=" , str(a+b))
+
+# МОЙ ВОЗРАСТ. Для заданого 0<=n<=200.Расматриваемого как возраст человека, вывести фразу вида: "Мне 21 год", "Мне 32 года"
+
+n = int (input("Введите возраст: "))
+print("мне " , n, end = " ")
+
+if n % 10 == 1:
+    print("год")
+elif n % 10 >= 1 and n % 10 <= 4:
+    print("года")
+else:
+    print("год")
---
+++ lesson_2/main.py
@@ -0,0 +1,50 @@
+IP_ADRESS = "198.168.133.12"
+# int - bul naturaldyk jana ters sandar
+intSan = 134783748579842937453959
+# float - bul tochka menen jazylgan sandar
+floatSan = 3.14
+# str (string) - bul sap, kaalagan sozdu jazsa bolot
+strSap = "Python language"
+# char - bul symbol bir gana tamga je san je symvol jazsak bolot
+charSymbol = 'A'
+# final - bul bir jolu gana ozgormogo maani bersek bolot
+CODE = "123565435"
+
+# print("Python ", intSan)
+# print("Python "+ str(intSan))
+# print("Python { intSan }")
+# print("3 + 5 = ", end=" result ")
+# print(8)
+# print("A code = ", chr(34))
+# for i in range(1,256):
+#     print(i, " = ", chr(i))
+
+# ctrl + ? (shit tin janynda)
+
+'''main.py
+jhbjhbkj
+kjnk
+kjnkj
+kjnk
+jhgjkk
+jjk
+'''
+# Matematicalyk esepter
+import math
+a = 47
+b = 3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+# 2**10 = 2*2*2*2*2*2*2*2*2*2 = 1024 
+print(2**10)
+print(34/13)
+print(34//13)
+# math.pow(negizgi san, daraja)
+# print(math.pow(a,b))
+
+
+
+
