52e204c9f0f828d563c9586d1f27d5a6a712cd5e
AuArgen <argenazizbekuulu3@gmail.com>
2022-10-15 22:04:29 +0600

    lesson 3

---
+++ lesson_3/main.py
@@ -0,0 +1,40 @@
+# a = int(input("a == "))
+# b = int(input("b == "))
+
+# if a > b:
+#     print("a sany cho eken b dan")
+#     if a % 2 == 0:
+#         print("Jup san")
+#     else:
+#         print("Tak san")    
+# elif a == b:
+#     print(" a barabar b ga")
+# else:
+#     print(" b chon eken a dan")
+
+
+# b = int(input("Adamdyn jashy = "))
+
+# if b <= 0:
+#     print("Joop jok")
+# elif b % 2 == 0:
+#     print("Aial kishi")
+# else:
+#     print("Erkek kishi")
+
+
+a = int(input("Adamdyn jashy = "))
+# if a < 18:
+#     if a % 2 == 0:
+#         print("Multfilmder")
+#     else:
+#         print("video sabaktar ")
+if a < 18 and a % 2 == 0:
+    print("Multfilmder")
+elif a < 18 and a % 2 == 1:
+    print("video sabaktar ")
+elif a < 55:
+    print("Kinolor video sabaktar")
+else:
+    print("Janylyktar jana kino")
+
